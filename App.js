136f841537f92147928896189ab224a6bfe1503b
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-12-29 14:26:04 +0100

    first comit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="text-red-500 text-4xl">Code with Sammy</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ navigation.js
@@ -0,0 +1,15 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+import { View, Text } from 'react-native';
+import React from 'react' //6.9k (gzipped: 2.7k)
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
