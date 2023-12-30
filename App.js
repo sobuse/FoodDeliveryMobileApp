f92a20f66fe9b293208dcc3022da74326c111533
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-12-30 23:21:35 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-red-500 text-4xl">Code with Sammy</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigation/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
