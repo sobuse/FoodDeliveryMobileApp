79925bd3e353c6b06fd99e7277796bef160286c9
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2023-12-30 23:21:31 +0100

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -3,12 +3,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 import { View, Text } from 'react-native';
 import React from 'react' //6.9k (gzipped: 2.7k)
+import RestaurantScreen from './screens/RestaurantScreen';
+import HomeScreen from './screens/HomeScreen';
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Restaurant" component={RestaurantScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
