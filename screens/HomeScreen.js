53f66ff2784ac0d6dd28778f0de0fc3e1ceafc6e
Ehueriken Samuel Obuse <59874300+sobuse@users.noreply.github.com>
2024-01-15 23:13:06 +0100

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,8 @@ import { StatusBar } from 'expo-status-bar'
 import * as Icon from "react-native-feather"
 import { themeColors } from '../theme'
 import Categories from '../components/categories'
+import { featured } from '../constants'
+import FeaturedRow from '../components/featuredRow'
 
 export default function HomeScreen() {
   return (
@@ -32,6 +34,22 @@ export default function HomeScreen() {
             }}>
                 {/*categories */}
                 <Categories />
+
+                {/* featured */}
+                <View className="mt-5">
+                  {
+                    [featured, featured, featured].map((item, index)=>{
+                      return(
+                        <FeaturedRow
+                        key={index}
+                        title={item.title}
+                        restaurants={item.restaurants}
+                        descroption={item.description}
+                        />
+                      )
+                    })
+                  }
+                </View>
 
             </ScrollView>
 
